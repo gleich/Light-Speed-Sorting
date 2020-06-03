7de90f6b97927522e398b7d2a332be636b6c975a
Matthew Gleich <matthewgleich@gmail.com>
2020-06-03 14:53:35 -0400

    ✨ Add filter to file getter
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- files/files.go
+++ files/files.go
@@ -3,22 +3,36 @@ package files
 import (
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/sirupsen/logrus"
 )
 
 // GetFiles ... Get all files recursively
 func GetFiles() []string {
-	files := []string{}
+	filePaths := []string{}
 	err := filepath.Walk(".", func(path string, _ os.FileInfo, err error) error {
 		if err != nil {
 			logrus.Fatal(err)
 		}
-		files = append(files, path)
+		filePaths = append(filePaths, path)
 		return nil
 	})
 	if err != nil {
 		logrus.Fatal(err)
 	}
-	return files
+
+	// Removing all . files and all folders
+	cleanedFilePaths := []string{}
+	for _, filePath := range filePaths {
+		fileOrDir, err := os.Stat(filePath)
+		if err != nil {
+			logrus.Fatal(err)
+		}
+		if !strings.HasPrefix(filePath, ".") && !fileOrDir.IsDir() {
+			cleanedFilePaths = append(cleanedFilePaths, filePath)
+		}
+	}
+
+	return cleanedFilePaths
 }
